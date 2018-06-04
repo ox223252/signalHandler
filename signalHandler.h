f6c826b28a57ea68df4e9ef935ae9f3366cfae2b
ox223252 <23252@laposte.net>
2018-06-04 20:10:19 +0200

    correct a cpoy/past mistake

--- signalHandler.h
+++ signalHandler.h
@@ -1,9 +1,10 @@
+#ifndef __SIGNALHANDLER_H__
+#define __SIGNALHANDLER_H__
+
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdint.h>
-
-#include "signalHandler.h"
 
 // SIGKILL : kill -9
 // SIGTERM : kill
@@ -14,184 +15,82 @@
 // SIGHUP : Ctrl+D	poke process
 // SIGALRM :
 
-static signalHandling *lg_ptr;
-static sigset_t g_sigsetSaved;
-
-static void catchSignals(int sig)
+typedef struct
 {
-	switch (sig)
-	{
-		case SIGINT:
-		{
-			if ( lg_ptr && lg_ptr->Int.func )
-			{
-				lg_ptr->Int.func ( lg_ptr->Int.arg );
-			}
-			break;
-		}
-		case SIGTERM:
-		{
-			if ( lg_ptr && lg_ptr->Term.func )
-			{
-				lg_ptr->Term.func ( lg_ptr->Term.arg );
-			}
-			break;
-		}
-		case SIGHUP:
-		{
-			if ( lg_ptr && lg_ptr->Hup.func )
-			{
-				lg_ptr->Hup.func ( lg_ptr->Hup.arg );
-			}
-			break;
-		}
-		case SIGQUIT:
-		{
-			if ( lg_ptr && lg_ptr->Quit.func )
-			{
-				lg_ptr->Quit.func ( lg_ptr->Quit.arg );
-			}
-			break;
-		}
-		case SIGSTOP:
-		{
-			if ( lg_ptr && lg_ptr->Stop.func )
-			{
-				lg_ptr->Stop.func ( lg_ptr->Stop.arg );
-			}
-			break;
-		}
-		case SIGCONT:
-		{
-			if ( lg_ptr && lg_ptr->Cont.func )
-			{
-				lg_ptr->Cont.func ( lg_ptr->Cont.arg );
-			}
-			break;
-		}
-		case SIGUSR1:
-		{
-			if ( lg_ptr && lg_ptr->Usr1.func )
-			{
-				lg_ptr->Usr1.func ( lg_ptr->Usr1.arg );
-			}
-			break;
-		}
-		case SIGUSR2:
-		{
-			if ( lg_ptr && lg_ptr->Usr2.func )
-			{
-				lg_ptr->Usr2.func ( lg_ptr->Usr2.arg );
-			}
-			break;
-		}
-		case SIGALRM:
-		{
-			if ( lg_ptr && lg_ptr->Alrm.func )
-			{
-				lg_ptr->Alrm.func ( lg_ptr->Alrm.arg );
-			}
-			break;
-		}
-		default:
-		{
-			// TODO: should all other signals be catched, and handled as SIGINT?
-			//
-			// Reason: currently, all other signals are just uncatched, and the OS kills us.
-			// The difference is that we could then close the open files properly.
-			break;
-		}
-	}
+	uint8_t Int:1,
+		Term:1,
+		Hup:1,
+		Quit:1,
+		Stop:1,
+		Cont:1,
+		Usr1:1,
+		Usr2:1,
+		Alrm:1;
 }
+signalFlag;
 
-void signalHandlerInit ( signalHandling *ptr )
+typedef struct
 {
-	if ( !ptr )
-	{
-		return;
-	}
-
-	lg_ptr = ptr;
-	if ( ptr->flag.Int )
-	{
-		signal ( SIGINT, catchSignals );
-	}
-	if ( ptr->flag.Term )
-	{
-		signal ( SIGTERM, catchSignals );
-	}
-	if ( ptr->flag.Hup )
-	{
-		signal ( SIGHUP, catchSignals );
-	}
-	if ( ptr->flag.Quit )
-	{
-		signal ( SIGQUIT, catchSignals );
-	}
-	if ( ptr->flag.Stop )
-	{
-		signal ( SIGSTOP, catchSignals );
-	}
-	if ( ptr->flag.Cont )
-	{
-		signal ( SIGCONT, catchSignals );
-	}
-	if ( ptr->flag.Usr2 )
-	{
-		signal ( SIGUSR2, catchSignals );
-	}
-	if ( ptr->flag.Usr2 )
-	{
-		signal ( SIGUSR2, catchSignals );
-	}
-	if ( ptr->flag.Alrm )
-	{
-		signal ( SIGALRM, catchSignals );
-	}
+	void (*func)( void * );
+	void * arg;
 }
+signalArgs;
 
-int signalHandlerSave ( sigset_t * ptr )
+typedef struct
 {
-	if ( ptr != NULL )
-	{
-		return ( sigprocmask ( SIG_SETMASK, NULL, ptr ) );
-	}
-	else
-	{
-		return ( sigprocmask ( SIG_SETMASK, NULL, &g_sigsetSaved ) );
-	}
+	signalFlag flag;
+	signalArgs Int;
+	signalArgs Term;
+	signalArgs Hup;
+	signalArgs Quit;
+	signalArgs Stop;
+	signalArgs Cont;
+	signalArgs Usr1;
+	signalArgs Usr2;
+	signalArgs Alrm;
 }
+signalHandling;
 
-int signalHandlerRestore ( sigset_t * ptr )
-{
-	if ( ptr != NULL )
-	{
-		return ( sigprocmask ( SIG_SETMASK, ptr, NULL ) );
-	}
-	else
-	{
-		return ( sigprocmask ( SIG_SETMASK, &g_sigsetSaved, NULL ) );
-	}
-}
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void signalHandlerInit ( signalHandling *ptr );
+/// \param [ in ] ptr: struct to init signal manager
+/// \biref
+////////////////////////////////////////////////////////////////////////////////
+void signalHandlerInit ( signalHandling *ptr );
 
-int signalHandlerBlock ( int signal )
-{
-	sigset_t set;
-	if ( sigaddset ( &set, signal ) )
-	{
-		return ( -1 );
-	}
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int signalHandlerSave ( sigset_t * ptr );
+/// \param [ out ] ptr: used to store the actual context of signal manager
+/// \biref this function will save the actual context of signal manger in ptr, 
+///     if ptr is NULL, it will save in internal var.
+/// \return if 0 then OK else see errno for more details
+////////////////////////////////////////////////////////////////////////////////
+int signalHandlerSave ( sigset_t * ptr );
 
-	return ( sigprocmask ( SIG_BLOCK, &set, NULL ) );
-}
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int signalHandlerRestore ( sigset_t * ptr );
+/// \param [ in ] ptr: used to store and old context of signal manager
+/// \biref this function will restored an old context, if ptr is NULL, it will 
+///     restore form internal var, signalHandlerSave() with NULL param should be
+///     called earlier.
+/// \return if 0 then OK else see errno for more details
+////////////////////////////////////////////////////////////////////////////////
+int signalHandlerRestore ( sigset_t * ptr );
 
-int signalHandlerUnBlock ( int signal )
-{
-	sigset_t set;
-	if ( sigaddset ( &set, signal ) )
-	{
-		return ( -1 );
-	}
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int signalHandlerBlock ( int signal );
+/// \param [ in ] signal: signal ID need to be Blocked
+/// \biref this function will block signal propagation.
+/// \return if 0 then OK else see errno for more details
+////////////////////////////////////////////////////////////////////////////////
+int signalHandlerBlock ( int signal );
 
-	return ( sigprocmask ( SIG_UNBLOCK, &set, NULL ) );
-}
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int signalHandlerUnBlock ( int signal );
+/// \param [ in ] signal: signal ID should be unblocked
+/// \biref this function will unblock signal propagation.
+/// \return if 0 then OK else see errno for more details
+////////////////////////////////////////////////////////////////////////////////
+int signalHandlerUnBlock ( int signal );
+
+#endif
